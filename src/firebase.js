e04c5a9a54ae053638e06d454036673d6f006e96
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-06-30 19:01:21 -0500

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCdqE8FqB73Nq1HI1CbQYF2_Vsy50UfECI',
+  authDomain: 'rememberme-c2d9f.firebaseapp.com',
+  projectId: 'rememberme-c2d9f',
+  storageBucket: 'rememberme-c2d9f.appspot.com',
+  messagingSenderId: '1098176943880',
+  appId: '1:1098176943880:web:3f321f68efebb29858bebd',
+};
+  // Initialize Firebase
+const firebaseInit = firebase.initializeApp(firebaseConfig);
+
+export default firebaseInit;
