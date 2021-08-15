969bfe4bf434d50df1b361c39f6c8770ea9a74c8
SSaenzC <66050169+SSaenzC@users.noreply.github.com>
2021-08-14 19:44:30 -0500

    modification app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 // import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -17,7 +17,7 @@ toast.configure();
 function App() {
   return (
     <div className="App">
-      <Router basename={window.location.pathname || ''}>
+      <Router basename={process.env.PUBLIC_URL }>
         <AuthProvider>
           <Switch>
             <PrivateRoute exact path="/" component={Dashboard} />
